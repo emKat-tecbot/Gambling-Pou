90d6137e277f54749a78e011b6b4d583fe83548e
emKat-tecbot <emmamolinahdez@gmail.com>
2026-06-14 00:34:59 +0000

    player.h construction

---
+++ player.h
@@ -0,0 +1,17 @@
+﻿#ifndef PLAYER_H
+#define PLAYER_H
+#include <string>
+using namespace std;
+
+class Player{
+   private:
+        string id;
+        int money;
+    public:
+        Player(string id, int money);
+        void work();
+        void care_for_pou();
+        void gamble();
+};
+
+#endif //PLAYER_H
