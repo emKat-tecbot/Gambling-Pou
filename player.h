3cdf4622fde7b4d6d7f21331284194ef6564ac7f
emKat-tecbot <emmamolinahdez@gmail.com>
2026-06-14 00:47:38 +0000

    Forward declaration of Pou class

--- player.h
+++ player.h
@@ -1,14 +1,17 @@
-﻿#ifndef PLAYER_H
+#ifndef PLAYER_H
 #define PLAYER_H
 #include <string>
 using namespace std;
 
+class Pou; // Forward declaration of Pou class to avoid circular dependency, since Player has a pointer to Pou and Pou might have a pointer to Player.
 class Player{
    private:
         string id;
         int money;
+        Pou* pou; // Pou determines whether player works and player must take care of pou
     public:
         Player(string id, int money);
+        ~Player();
         void work();
         void care_for_pou();
         void gamble();
